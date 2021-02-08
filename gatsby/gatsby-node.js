8fa5cddf320a3aa04680645249366524fdbc3c5c
Devon Neill <devon.neill@prodigyfinance.com>
2021-02-08 19:33:36 +0200

    Attempt file clean up

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -1,12 +1,69 @@
-async function turnPizzasIntoPages(params) {
+import path from 'path';
+
+async function turnPizzasIntoPages({ graphql, actions }) {
   // 1. Get a template for this page
+  const pizzaTemplate = path.resolve('./src/templates/Pizza.js');
   // query all pizzas
+  const { data } = await graphql(`
+    query {
+      pizzas: allSanityPizza {
+        nodes {
+          name
+          slug {
+            current
+          }
+        }
+      }
+    }
+  `);
+  console.log(data);
   // loop over each pizza and create page for that pizza
+  data.pizzas.nodes.forEach((pizza) => {
+    actions.createPage({
+      // what is the url for this new page?
+      path: `pizza/${pizza.slug.current}`,
+      component: pizzaTemplate,
+      context: {
+        slug: pizza.slug.current,
+      },
+    });
+  });
 }
 
-export async function createPages() {
-  // create pages dunamically
-  // pizzas
-  // toppings
+async function turnToppingsIntoPages({ graphql, actions }) {
+  console.log('Turning the toppings into pages');
+  // 1. Get a template for this page
+  const toppingTemplate = path.resolve('./src/pages/pizzas.js');
+  // 2. query all the toppings
+  const { data } = await graphql(`
+    query {
+      toppings: allSanityTopping {
+        nodes {
+          name
+          id
+        }
+      }
+    }
+  `);
+  // 3. createPage for that topping
+  data.toppings.nodes.forEach((topping) => {
+    console.log(`creating page for topping`, topping.name);
+    actions.createPage({
+      path: `topping/${topping.name}`,
+      component: toppingTemplate,
+      context: {
+        topping: topping.name,
+        toppingRegex: `/${topping.name}/i`,
+      },
+    });
+  });
+  // 4. pass topping data to pizza.js
+}
+
+export async function createPages(params) {
+  await Promise.all([
+    turnPizzasIntoPages(params),
+    turnToppingsIntoPages(params),
+  ]);
   // slicemasters
 }
