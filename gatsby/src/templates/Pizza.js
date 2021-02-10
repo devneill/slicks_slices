c5498061a5a84ad939cc24a688bf510bf9ebbee5
Devon Neill <devon.neill@prodigyfinance.com>
2021-02-10 08:04:20 +0200

    Add missing import

--- gatsby/src/templates/Pizza.js
+++ gatsby/src/templates/Pizza.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Img from 'gatsby-image';
 import styled from 'styled-components';
+import { graphql } from 'gatsby';
 
 const PizzaGrid = styled.div`
   display: grid;
