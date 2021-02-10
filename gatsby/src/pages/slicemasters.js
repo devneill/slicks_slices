f5b99ab8cbcc2f82a61d022042ed454f1c140099
Devon Neill <devon.neill@prodigyfinance.com>
2021-02-10 08:39:57 +0200

    Query data for slicemasters page

--- gatsby/src/pages/slicemasters.js
+++ gatsby/src/pages/slicemasters.js
@@ -1,9 +1,40 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 
-export default function SlicemastersPage() {
+export default function SlicemastersPage({ data }) {
+  console.log(data);
   return (
     <>
       <p>Hey! I'm the Slicemasters page</p>
+      <div>
+        {data.slicemasters.nodes.map((slicemaster) => (
+          <p>{slicemaster.name}</p>
+        ))}
+      </div>
     </>
   );
 }
+
+export const query = graphql`
+  query {
+    slicemasters: allSanityPerson {
+      nodes {
+        name
+        description
+        slug {
+          current
+        }
+        image {
+          asset {
+            fixed(width: 200, height: 200) {
+              ...GatsbySanityImageFixed
+            }
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
