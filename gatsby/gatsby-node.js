83a3bd89a5b833dd94f72f90af97a1155a833384
Devon Neill <devon.neill@prodigyfinance.com>
2021-02-16 16:56:00 +0200

    Add query and page variables

--- gatsby/gatsby-node.js
+++ gatsby/gatsby-node.js
@@ -87,6 +87,32 @@ async function fetchBeersAndTurnIntoNodes({
   }
 }
 
+async function turnSlicemastersIntoPages({ graphql, actions }) {
+  // query all slicemasters
+  const { data } = await graphql(`
+    query {
+      slicemasters: allSanityPerson {
+        totalCount
+        nodes {
+          name
+          id
+          slug {
+            current
+          }
+        }
+      }
+    }
+  `);
+  // TODO turn each slicemaster into their own page
+  // figure out how many pages there are based on how many slicemasters there are per page
+  const pageSize = parseInt(process.env.GATSBY_PAGE_SIZE);
+  const pageCount = Math.ceil(data.slicemasters.totalCount / pageSize);
+  console.log(
+    `There are ${data.slicemasters.totalCount} total people. And we have ${pageCount} pages with ${pageSize} per page`
+  );
+  //  loop over from 1 to n and create the pages for them
+}
+
 export async function sourceNodes(params) {
   // fetch list of beers and source them into our gatsby API
   await Promise.all([fetchBeersAndTurnIntoNodes(params)]);
