9dd0e1c64e1048ae3428ddb260cb768cd88070d9
Devon Neill <dev@neillfam.co.za>
2021-04-02 15:01:46 +0200

    Update order page and pizza page to use Order context state

--- gatsby/src/utils/usePizza.js
+++ gatsby/src/utils/usePizza.js
@@ -1,8 +1,12 @@
-import { useState } from 'react';
+import { useContext } from 'react';
+import OrderContext from '../components/OrderContext';
 
 export default function usePizza({ pizzas, inputs }) {
   // create state to hold order
-  const [order, setOrder] = useState([]);
+  // We got rid of this lnie cuz we moved the usetate up to the provider
+  // const [order, setOrder] = useState([]);
+  // Now we have access to both our state and our updater function (setOrder) via context
+  const [order, setOrder] = useContext(OrderContext);
   // make a function to add things to order
   function addToOrder(orderedPizza) {
     setOrder([...order, orderedPizza]);
