19f82730c47afd1f3b17a106024f09d20d24c109
Devon Neill <dev@neillfam.co.za>
2021-04-03 12:07:49 +0200

    Populate order email

--- gatsby/functions/placeOrder/placeOrder.js
+++ gatsby/functions/placeOrder/placeOrder.js
@@ -1,5 +1,28 @@
 const nodemailer = require('nodemailer');
 
+function generateOrderEmail({ order, total }) {
+  return `<div>
+    <h2>Your recent order for ${total}</h2>
+    <p>Please start walking over, we will have your order ready in the next 20mins.</p>
+    <ul>${order
+      .map(
+        (item) =>
+          `<li>
+            <img src="${item.thumbnail}" alt="${item.name}"/> 
+            ${item.size} ${item.name} - ${item.price}
+        </li>`
+      )
+      .join('')}
+     </ul>
+    <p>Your total is <strong>${total}</strong> due at pickup</p> 
+    <style>
+        ul {
+        list-style: none;
+       }
+    </style>
+    </div>`;
+}
+
 // create a transport for nodemailer
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
@@ -11,12 +34,33 @@ const transporter = nodemailer.createTransport({
 });
 
 exports.handler = async (event, context) => {
+  const body = JSON.parse(event.body);
+  console.log(body);
+  // validate info coming in is correct
+  const requiredFields = ['email', 'name', 'order'];
+
+  for (const field of requiredFields) {
+    console.log(`Checking that ${field} is good`);
+    if (!body[field]) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: `Oops! You are missing the ${field} field`,
+        }),
+      };
+    }
+  }
+
+  // send the email
+
+  // send the success or error message
+
   // Test send an email
   const info = await transporter.sendMail({
-    from: 'Slicks Slices <slick@example.com',
-    to: 'orders@example.com',
+    from: 'Slicks Slices <slick@example.com>',
+    to: `${body.name} <${body.email}>, orders@example.com`,
     subject: 'New Order!',
-    html: `<p>Your new pizza order is here!</p>`,
+    html: generateOrderEmail({ order: body.order, total: body.total }),
   });
-  return { statusCode: 200, body: JSON.stringify(info) };
+  return { statusCode: 200, body: JSON.stringify({ message: 'Success' }) };
 };
